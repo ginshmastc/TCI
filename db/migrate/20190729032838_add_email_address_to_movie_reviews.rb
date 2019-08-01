539ee766c003ae946b6a4e12c82fe7a4ea56d51a
Erik Holden <eholden@conviva.com>
2019-08-01 15:50:29 -0700

    up

--- db/migrate/20190729032838_add_email_address_to_movie_reviews.rb
+++
@@ -1,5 +0,0 @@
-class AddEmailAddressToMovieReviews < ActiveRecord::Migration[5.2]
-  def change
-    add_column :movie_reviews, :email_address, :string
-  end
-end
--- db/migrate/20190729033236_add_email_address_to_tv_reviews.rb
+++
@@ -1,5 +0,0 @@
-class AddEmailAddressToTvReviews < ActiveRecord::Migration[5.2]
-  def change
-    add_column :tv_reviews, :email_address, :string
-  end
-end
